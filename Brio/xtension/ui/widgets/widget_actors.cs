d10327576be80eff4b826f23b91ea2277249222a
cfsen <103598332+cfsen@users.noreply.github.com>
2025-12-23 15:34:10 +0100

    feat: actor widget spawn/despawn

--- Brio/xtension/ui/widgets/widget_actors.cs
+++ Brio/xtension/ui/widgets/widget_actors.cs
@@ -1,11 +1,19 @@
+using System;
+using Brio.Capabilities.Actor;
+using Brio.Entities;
+using Brio.Entities.Actor;
 using Brio.Entities.Core;
 using Brio.UI.Entitites;
+using Brio.UI.Widgets.Core;
+using Brio.Xtension.helpers;
+using Dalamud.Interface;
 
 namespace Brio.Xtension.ui.widgets;
 
 public class XTWidgetActors(
         Entity _actorContainer,
-        EntityHierarchyView entView
+        EntityHierarchyView entView,
+        EntityManager entMan
         ) : XTWidget {
 
     private readonly Entity ActorContainer = _actorContainer;
@@ -13,7 +21,46 @@ public class XTWidgetActors(
     public override XTWidgetCategory WidgetCategory => XTWidgetCategory.Posing;
 
     public override void Draw(){
-        ActorContainer.DrawContextButton();
+        if(ActorContainer.IsLoading) return;
+
+        DrawHeader();
         entView.Draw(ActorContainer);
+
     }
+
+    private void DrawHeader(){
+        XUI.WidgetHeader("Actors", FontAwesomeIcon.PersonRays);
+
+        Action pop_enable = XUI.EnableIf(ActorContainer.HasCapability<ActorContainerCapability>());
+
+        XUI.Button(SpawnActor, FontAwesomeIcon.Plus, XUI.Size.BtnMid, XUI.Inline.Yes);
+        XUI.Button(CloneActor, FontAwesomeIcon.Copy, XUI.Size.BtnMid, XUI.Inline.Yes);
+        XUI.Button(SpawnProp, FontAwesomeIcon.PlusCircle, XUI.Size.BtnMid, XUI.Inline.Yes);
+        XUI.Button(TargetActor, FontAwesomeIcon.Bullseye, XUI.Size.BtnMid, XUI.Inline.Yes);
+        XUI.Button(DestroyActor, FontAwesomeIcon.Minus, XUI.Size.BtnMid);
+
+        pop_enable();
+    }
+
+    private void SpawnActor()
+        => ActorContainer.GetCapability<ActorContainerCapability>()?.CreateCharacter(false, true, true);
+
+    private void SpawnProp()
+        => ActorContainer.GetCapability<ActorContainerCapability>()?.CreateProp(true);
+
+    private void CloneActor(){
+        if(entMan.SelectedEntity is not ActorEntity actor) return;
+        ActorContainer.GetCapability<ActorContainerCapability>()?.CloneActor(actor, false);
+    }
+
+    private void DestroyActor(){
+        if(entMan.SelectedEntity is not ActorEntity actor) return;
+        ActorContainer.GetCapability<ActorContainerCapability>()?.DestroyCharacter(actor);
+    }
+
+    private void TargetActor(){
+        if(entMan.SelectedEntity is not ActorEntity actor) return;
+        ActorContainer.GetCapability<ActorContainerCapability>()?.Target(actor);
+    }
+
 }
